2e620f298f838b246e73e0eec95284b6a824b879
mbs-code <mbscodec@gmail.com>
2022-05-22 12:44:33 +0900

    add: tailwind を導入
    - tailwind
    - daisy ui
    - hero icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,22 @@ import eslintPlugin from 'vite-plugin-eslint'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   srcDir: 'src/',
-
   ssr: false,
   target: 'static',
 
+  css: ['@/assets/styles/tailwind.css'],
+
   vite: {
     plugins: [eslintPlugin()]
-  }
+  },
+
+  build: {
+    postcss: {
+      postcssOptions: {
+        plugins: {
+          tailwindcss: {},
+        },
+      },
+    },
+  },
 })
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: [
+    './src/**/*.{vue,js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
