426698f6d604ba985e009af6f3dfc3f57991ccdb
mbs-code <mbscodec@gmail.com>
2022-05-23 00:58:52 +0900

    mod: 参照設定の整理

--- src-tauri/src/command/site.rs
+++ src-tauri/src/command/site.rs
@@ -58,19 +58,19 @@ pub fn get(page_id: &i64) -> Result<Site, Box<dyn Error>> {
 }
 
 #[tauri::command]
-pub fn create(url: &String, title: &Option<String>) -> Result<Site, Box<dyn Error>> {
+pub fn create(url: String, title: Option<String>) -> Result<Site, Box<dyn Error>> {
     let conn = get_conn()?.lock()?;
 
-    let now = &chrono_now();
+    let now = chrono_now();
     let sql = SqlBuilder::insert_into("sites")
         .field("url")
         .field("title")
         .field("created_at")
         .field("updated_at")
-        .values(&["?", "?", &quote(now), &quote(now)])
+        .values(&["?", "?", &quote(&now), &quote(&now)])
         .sql()?
-        .bind(url)
-        .bind(title);
+        .bind(&url)
+        .bind(&title);
 
     #[cfg(debug_assertions)]
     println!("{:?}", &sql);
@@ -88,18 +88,18 @@ pub fn create(url: &String, title: &Option<String>) -> Result<Site, Box<dyn Erro
 }
 
 #[tauri::command]
-pub fn update(site_id: &i64, url: &String, title: &Option<String>) -> Result<Site, Box<dyn Error>> {
+pub fn update(site_id: &i64, url: String, title: Option<String>) -> Result<Site, Box<dyn Error>> {
     let conn = get_conn()?.lock()?;
 
-    let now = &chrono_now();
+    let now = chrono_now();
     let sql = SqlBuilder::update_table("sites")
         .set("url", "?")
         .set("title", "?")
-        .set("updated_at", &quote(now))
+        .set("updated_at", &quote(&now))
         .and_where("id = ?".bind(site_id))
         .sql()?
-        .bind(url)
-        .bind(title);
+        .bind(&url)
+        .bind(&title);
 
     #[cfg(debug_assertions)]
     println!("{:?}", &sql);
