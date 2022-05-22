35605e7db027790ec05033aeb5925153e6a90e0a
mbs-code <mbscodec@gmail.com>
2022-05-22 23:52:37 +0900

    add: update と delete を実装

--- src-tauri/src/command/site.rs
+++ src-tauri/src/command/site.rs
@@ -52,3 +52,47 @@ pub fn create(url: String, title: Option<String>) -> Result<Site, Box<dyn Error>
     let new_site = get(&site_id)?;
     Ok(new_site)
 }
+
+#[tauri::command]
+pub fn update(site_id: i64, url: String, title: Option<String>) -> Result<Site, Box<dyn Error>> {
+    let conn = get_conn()?.lock()?;
+
+    let now = chrono_now();
+    let sql = SqlBuilder::update_table("sites")
+        .set("url", "?")
+        .set("title", "?")
+        .set("updated_at", &quote(&now))
+        .and_where("id = ?".bind(&site_id))
+        .sql()?
+        .bind(&url)
+        .bind(&title);
+
+    #[cfg(debug_assertions)]
+    println!("{:?}", &sql);
+
+    let _ = conn.execute(&sql, [])?;
+
+    #[cfg(debug_assertions)]
+    println!("site_id: {}", &site_id);
+
+    // 再取得のため開放
+    drop(conn);
+    let new_site = get(&site_id)?;
+    Ok(new_site)
+}
+
+#[tauri::command]
+pub fn delete(site_id: i64) -> Result<(), Box<dyn Error>> {
+    let conn = get_conn()?.lock()?;
+
+    let sql = SqlBuilder::delete_from("sites")
+        .and_where("id = ?".bind(&site_id))
+        .sql()?;
+
+    #[cfg(debug_assertions)]
+    println!("{:?}", &sql);
+
+    let _ = conn.execute(&sql, [])?;
+
+    Ok(())
+}
