5af6fb120bae7329b69cb74e06e758480b0eca90
mbs-code <mbscodec@gmail.com>
2022-05-22 11:41:36 +0900

    add: eslint と editor config の追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,14 @@
-import { defineNuxtConfig } from "nuxt";
+import { defineNuxtConfig } from 'nuxt'
+import eslintPlugin from 'vite-plugin-eslint'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  srcDir: "src/",
+  srcDir: 'src/',
 
   ssr: false,
-  // target: "static",
-});
+  target: 'static',
+
+  vite: {
+    plugins: [eslintPlugin()]
+  }
+})
