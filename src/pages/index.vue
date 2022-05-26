36b594d59718fe9b019472ca3faab2b73f4b5c39
mbs-code <mbscodec@gmail.com>
2022-05-26 20:46:43 +0900

    add: layout を実装

---
+++ src/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="~assets/logo.png">
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <h1 class="pl-24 pt-12 text-3xl text-red-600 underline">
+    Hello world!
+  </h1>
+
+  <button class="btn">
+    Hello daisyUI
+  </button>
+
+  <!-- <div>{{ sites[5] }}</div> -->
+
+  <!-- <NuxtWelcome /> -->
+</template>
+
+<style>
+#__nuxt {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
